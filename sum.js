0c0ff27fd88ca7e12ee1dcd9be7bccbb69694bc8
Sergei <39593488+PylSerg@users.noreply.github.com>
2022-06-12 21:50:37 +0300

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,14 +1,3 @@
-// const numbers1 = 10;
-// const numbers2 = [5, 6, 3];
-// const numbers3 = [[1, 5, 4], [2, 8]];
-// const numbers4 = [['1', '5,6', '4, 1'], ['2 3', '8']];
-// const numbers5 = ['14', '15,9', '1, 2', '5 4'];
-// const numbers6 = '9, 6, 3';
-// const numbers7 = '8,6,1';
-// const numbers8 = '8 9 10';
-
-
-
 function sum(...args) {
   let sum = 0;
   
